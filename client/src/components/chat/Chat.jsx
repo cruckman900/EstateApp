968f763f545b8ab156fa70825c428936fe50730b
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2024-06-06 21:46:45 -0400

    finished profile page and project UI

---
+++ client/src/components/chat/Chat.jsx
@@ -0,0 +1,151 @@
+import { useState } from 'react';
+import './chat.scss';
+
+function Chat() {
+    const [chat, setChat] = useState(true);
+
+    return (
+        <div className="chat">
+            <div className="messages">
+                <h1>Messages</h1>
+                <div className="message">
+                    <img src="https://www.seekpng.com/png/detail/60-604032_face-businessman-png-dummy-images-for-testimonials.png" alt="" />
+                    <span>John Doe</span>
+                    <p>Lorem ipsum dolor sit amet...
+                    </p>
+                </div>
+                <div className="message">
+                    <img src="https://www.seekpng.com/png/detail/60-604032_face-businessman-png-dummy-images-for-testimonials.png" alt="" />
+                    <span>John Doe</span>
+                    <p>Lorem ipsum dolor sit amet...
+                    </p>
+                </div>
+                <div className="message">
+                    <img src="https://www.seekpng.com/png/detail/60-604032_face-businessman-png-dummy-images-for-testimonials.png" alt="" />
+                    <span>John Doe</span>
+                    <p>Lorem ipsum dolor sit amet...
+                    </p>
+                </div>
+                <div className="message">
+                    <img src="https://www.seekpng.com/png/detail/60-604032_face-businessman-png-dummy-images-for-testimonials.png" alt="" />
+                    <span>John Doe</span>
+                    <p>Lorem ipsum dolor sit amet...
+                    </p>
+                </div>
+                <div className="message">
+                    <img src="https://www.seekpng.com/png/detail/60-604032_face-businessman-png-dummy-images-for-testimonials.png" alt="" />
+                    <span>John Doe</span>
+                    <p>Lorem ipsum dolor sit amet...
+                    </p>
+                </div>
+                <div className="message">
+                    <img src="https://www.seekpng.com/png/detail/60-604032_face-businessman-png-dummy-images-for-testimonials.png" alt="" />
+                    <span>John Doe</span>
+                    <p>Lorem ipsum dolor sit amet...
+                    </p>
+                </div>
+                <div className="message">
+                    <img src="https://www.seekpng.com/png/detail/60-604032_face-businessman-png-dummy-images-for-testimonials.png" alt="" />
+                    <span>John Doe</span>
+                    <p>Lorem ipsum dolor sit amet...
+                    </p>
+                </div>
+            </div>
+            {chat && (
+                <div className="chatBox">
+                    <div className="top">
+                        <div className="user">
+                            <img src="https://www.seekpng.com/png/detail/60-604032_face-businessman-png-dummy-images-for-testimonials.png" alt="" />
+                            John Doe
+                        </div>
+                        <span className="close" onClick={() => setChat(null)}>X</span>
+                    </div>
+                    <div className="center">
+                        <div className="chatMessage own">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage own">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage own">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage own">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage own">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage own">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage own">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage own">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage own">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                        <div className="chatMessage own">
+                            <p>Lorem ipsum dolor sit amet</p>
+                            <span>1 hour ago</span>
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <textarea></textarea>
+                        <button>Send</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Chat;
