d3c9ffc288c2317d6cfac8c792dc3cd3bcb32b9d
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2024-07-29 17:53:11 -0400

    modified cookie info to include user details

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -64,11 +64,13 @@ export const login = async (req, res) => {
             { expiresIn: age }
         );
 
+        const { password: userPassword, ...userInfo } = user;
+
         res.cookie("token", token, {
             httpOnly: true,
             // secure: true,
             maxAge: age,
-        }).status(200).json({ message: "Login Successful" });
+        }).status(200).json(userInfo);
 
     } catch(err) {
         console.log(err);
