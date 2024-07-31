3c4ff46b49fdd7053aa212a62f07d698db7b5948
Christopher L Ruckman <cruckman900@cardinal.wju.edu>
2024-07-31 09:37:31 -0400

    added middleware -verifyToken.  performing authentication

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -58,7 +58,8 @@ export const login = async (req, res) => {
 
         const token = jwt.sign(
             {
-                id: user.id
+                id: user.id,
+                isAdmin: true,
             },
             process.env.JWT_SECRET_KEY,
             { expiresIn: age }
